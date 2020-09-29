62fb08edd27bf0785e889a2bb0308a15d12c5a1b
csemler <csemler@wisc.edu>
2020-09-29 02:00:51 +0000

    LogInAndRegister.java

--- LogInAndRegister.java
+++ LogInAndRegister.java
@@ -2,24 +2,29 @@
 // Name: Christopher Semler
 // Email: csemler@wisc.edu
 // Team: CG
-// Role: DataWranger2
+// Role: DataWrangler2
 // TA: Yeping
 // Lecturer: Florian
-// Notes to Grader: none
+// Notes to Grader: nope
 
-public class LogInDatabase
+public class LogInAndRegister
 {
 	private HashTable<String, User> userTable;
-
+	
+	//method to securely encrypt password
+	//take each character and multiply by two
 	public String encryption(String password)
 	{
-		char[] chars = password.toCharArray();
-		for (int i = 0; i < chars.length; i++)
-			chars[i] += 8;
+		char[] passChar = password.toCharArray();
+		for (int i = 0; i < passChar.length; i++)
+			passChar[i] += passChar[i];
 
-		return String.valueOf(chars);
+		return String.valueOf(passChar);
 	}
 
+	//checks to see if user and password are in database and match
+	//returns false if user doesn't exist or password is incorrect
+	//returns true if user exists and password matches
 	public boolean logIn(String username, String password)
 	{
 		if (isUserExist(username) == false)
@@ -28,6 +33,8 @@ public class LogInDatabase
 		return userTable.get(username).getPasswordEncrypted().equals(encrypPass);
 	}
 
+	//adds user to HashTable
+	//returns true if successful
 	public boolean addUser(String username, String password, boolean isAdmin)
 	{
 		if (isUserExist(username) == true)
@@ -39,11 +46,15 @@ public class LogInDatabase
 		return true;
 	}
 
+	//checks to see if user exists based on username
+	//returns result
 	public boolean isUserExist(String username)
 	{
 		return userTable.containsKey(username);
 	}
-
+	
+	//checks to see if user is admin
+	//returs result
 	public boolean isAdmin(String username)
 	{
 		return userTable.get(username).isAdmin();
